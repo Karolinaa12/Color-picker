4abb5cacf5487754dc1a2c19abbd24964baa92b6
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2023-06-26 16:20:20 +0200

    Added hex to rgb functionality

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 let colorInput = document.querySelector("#color");
 let hexInput = document.querySelector("#hex");
+let rgbInput = document.querySelector("#rgb");
 let header = document.querySelector("h1");
 
 colorInput.addEventListener("input", () => {
   let color = colorInput.value;
   hexInput.value = color;
   header.style.color = color;
+
+  const hexColor = hexInput.value;
+  const rgbColor = hexToRgb(hexColor);
+  console.log(rgbColor);
+  rgbInput.value = rgbColor;
 });
+
+function hexToRgb(hex) {
+  hex = hex.replace("#", "");
+
+  const r = parseInt(hex.substring(0, 2), 16);
+  const g = parseInt(hex.substring(2, 4), 16);
+  const b = parseInt(hex.substring(4, 6), 16);
+
+  return `${r}, ${g}, ${b}`;
+}
