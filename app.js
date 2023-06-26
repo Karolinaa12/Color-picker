670f5ddd6df5bffb4d330d58fa3077c900abf040
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2023-06-26 16:08:58 +0200

    Changing color of the header

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let colorInput = document.querySelector("#color");
 let hexInput = document.querySelector("#hex");
+let header = document.querySelector("h1");
 
 colorInput.addEventListener("input", () => {
   let color = colorInput.value;
   hexInput.value = color;
+  header.style.color = color;
 });
